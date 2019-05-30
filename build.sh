c75973550a89c1b58a7b50beb33fe1a1148d2f6c
tongshukang <tongshukang@caicloud.io>
2019-05-30 14:27:44 +0800

    fix build script

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@ CGO_ENABLED="0"
 
 GOOS="darwin"
 GOARCH="amd64"
-go build -o tskssh_${GOOS}_${GOARCH}
+go build -o bin/tskssh_${GOOS}_${GOARCH} cmd/tskssh.go
 
 GOOS="linux"
 GOARCH="amd64"
-go build -o tskssh_${GOOS}_${GOARCH}
+go build -o bin/tskssh_${GOOS}_${GOARCH} cmd/tskssh.go
